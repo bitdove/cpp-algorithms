fcbc42266e3642d68b0eb4cd6c40cf3d6e77294a
MasonCodingHere <38603431+MasonCodingHere@users.noreply.github.com>
2024-09-28 13:15:39 +0800

    fix ci.yml

---
+++ data-structures/LinkedList/build/CMakeFiles/test_LinkedList.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/mason/cpp-algorithms/data-structures/linked-list/bin/test_LinkedList"
+  "/home/mason/cpp-algorithms/data-structures/linked-list/bin/test_LinkedList.pdb"
+  "CMakeFiles/test_LinkedList.dir/__test__/test_LinkedList.cpp.o"
+  "CMakeFiles/test_LinkedList.dir/__test__/test_LinkedList.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_LinkedList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
